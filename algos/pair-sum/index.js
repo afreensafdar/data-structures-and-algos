22a2226f478cc7f44ba26f835e3838f08143ecf4
afreensafdar <afreen.leo502@gmail.com>
2022-03-09 16:47:42 -0600

    pair-sum algorithm add

--- algos/pair-sum/index.js
+++ algos/pair-sum/index.js
@@ -1,5 +1,18 @@
 function pairSum(arr, sum){
 	// Complete the function!
+	
+	for(let i = 0; i < arr.length - 1; i++){
+		for(let j = i + 1; j < arr.length; j++){
+			if(arr[i] + arr[j] == sum){
+				return true
+			}
+		}
+	}
+	return false
 }
 
+
 module.exports = pairSum
+
+//Time Complexity: O(n2) 
+//Auxiliary Space: O(1)
